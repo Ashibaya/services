0053ccb49aacb800140e2f5bb2f33d66a5ff812c
Ashibaya <coolday13@mail.ru>
2021-03-03 11:58:03 +0900

    Set api router

--- routers/api.py
+++ routers/api.py
@@ -1,7 +1,44 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from typing import Optional
+from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 router = APIRouter()
 
-@router.post("/login")
-async def getLogin():
-    return {'dsd':'asda'}
+
+engine = create_engine(
+    "mssql+pyodbc://sa:gjghj,eqgjl,thb@172.16.4.58:1433/General?driver=ODBC+Driver+17+for+SQL+Server")
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    return Session()
+
+
+@router.get("/")
+async def an_endpoint_using_sql(db=Depends(get_db)):
+    # ...
+    # do some SQLAlchemy
+    # ...
+    return {"msg": "an exceptionally successful operation!"}
+
+
+@router.get("/getCities/region-id={region_id}/")
+async def get_city(region_id: int, db=Depends(get_db)):
+    return db.execute(f"SELECT id, name FROM General.dbo.vCities WHERE id_region = {region_id}").fetchall()
+
+
+@router.get("/getCities/")
+async def get_cities(db=Depends(get_db)):
+    return db.execute("SELECT id, name FROM General.dbo.vCities").fetchall()
+
+
+@router.get("/getAddress/city-id={city_id}/")
+async def get_address(city_id: int, db=Depends(get_db)):
+    return db.execute(f"EXEC General.dbo.getAddress @city_id = {city_id}").fetchall()
+
+
+@router.get("/getRegions/")
+async def get_regions(db=Depends(get_db)):
+    return db.execute(f"SELECT id, name FROM General.dbo.vRegions").fetchall()
--- run.py
+++ run.py
@@ -2,9 +2,20 @@ from fastapi.responses import HTMLResponse
 from fastapi import Depends, FastAPI, Header, HTTPException
 from fastapi.staticfiles import StaticFiles
 from routers import admin
-import uvicorn 
+from routers import api
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse, Response, UJSONResponse
+import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/app/static", StaticFiles(directory="app/static"), name="static")
 
@@ -13,8 +24,16 @@ app.include_router(
     prefix="/admin",
     tags=["admin"],
     responses={404: {"description": "Not found"}},
-    )
+)
+
+app.include_router(
+    api.router,
+    prefix="/api",
+    tags=["api"],
+    responses={404: {"description": "Not found"}},
+)
 
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", host="127.0.0.1", port=5000, log_level="info", debug = True)
+    uvicorn.run("run:app", host="127.0.0.1", port=5000,
+                log_level="info", debug=True)
